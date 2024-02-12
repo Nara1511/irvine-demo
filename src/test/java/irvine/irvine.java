5cbdf16935b14ace9d4a23bed75273ff79165e21
Nargiz Aliyeva <nrgz.ava@gmail.com>
2024-02-11 21:31:09 -0800

    initial commit

---
+++ src/test/java/irvine/irvine.java
@@ -0,0 +1,18 @@
+package irvine;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class irvine {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        Faker faker = new Faker();
+        System.out.println(faker.business().creditCardNumber());
+        System.out.println(faker.chuckNorris().fact());
+
+    }
+
+    }
+
